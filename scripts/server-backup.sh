492e3e9c7ad787d9da251ff77ff9868be3ad3451
Joe Nyland <joenyland@me.com>
2010-06-17 20:23:22 +0000

    Removed server-backup.sh

--- scripts/server-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Server Backup Script
-
-cd /mnt/usb_backup/Backups/Server || exit
-tar cvpjf $HOSTNAME-`date +%F`.tar.bz2 --exclude=/proc --exclude=/lost+found --exclude=/cdrom --exclude=/media --exclude=/mnt --exclude=/sys --exclude=/dev /
-
