7a6d872fe323b517875673e55458c64e210699d5
Joe Nyland <joenyland@me.com>
2010-06-22 11:17:52 +0000

    Added disk-space utility script.

---
+++ scripts/disk-space.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Disk space analyser script (using du).
+
+echo
+echo There will be a delay while the local filesystem is scanned.
+echo
+echo Script now starting.
+echo 
+echo Please wait...
+echo 
+echo 
+gksudo du || sudo du -hx --max-depth=1 /
