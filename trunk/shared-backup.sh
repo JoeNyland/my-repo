90b05250dd9d41d15da7d1d63b2b546bedfee0a4
Joe Nyland <joenyland@me.com>
2010-11-16 13:04:04 +0000

    Changed exclude rule to correct format for Blu Ray movies directory.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -2,12 +2,12 @@
 # Shared files backup script to backup files from /mnt/server to 's iMac
 
 SHARED_DIR=/mnt/shared
-MAC_BACKUP=/mnt/mac-backup
-_BACKUP=/mnt/-backup
+MAC_BACKUP=/mnt/mac_backup
+_BACKUP=/mnt/_backup
 
-rsync -vruEthim --progress --log-file=$SHARED_DIR/.shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Server/
-rsync -vruEthim --progress --log-file=$SHARED_DIR/.shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Server/ --exclude=$SHARED_DIR/Movies/Blu\ Ray\ Movies/
-rsync -vruEthim --progress --log-file=$SHARED_DIR/.shared-backup.log $SHARED_DIR/Applications $_BACKUP/
-rsync -vruEthim --progress --log-file=$SHARED_DIR/.shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/
-rsync -vruEthim --progress --log-file=$SHARED_DIR/.shared-backup.log $SHARED_DIR/Music $_BACKUP/
-rsync -vruEthim --progress --log-file=$SHARED_DIR/.shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies/ $_BACKUP/
+rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared
+rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies"
+#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Applications $_BACKUP/
+#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/
+#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Music $_BACKUP/
+#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies/ $_BACKUP/
