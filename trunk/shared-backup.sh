7e986fc13670ba0e2be73863783da7547fb18fbb
Joe Nyland <joenyland@me.com>
2010-11-16 14:23:29 +0000

    Enabled backups

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -7,7 +7,7 @@ _BACKUP=/mnt/_backup
 
 rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared
 rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies"
-#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Applications $_BACKUP/
-#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/
-#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Music $_BACKUP/
-#rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies/ $_BACKUP/
+rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Applications $_BACKUP/
+rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/
+rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Music $_BACKUP/
+rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies/ $_BACKUP/
