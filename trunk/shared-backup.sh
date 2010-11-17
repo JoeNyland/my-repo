b191a2ac846797f3a2e38c591f1457c9c60c4c4a
Joe Nyland <joenyland@me.com>
2010-11-17 22:45:52 +0000

    shared-backup.sh now logs Rsync progress to /var/log/shared-backup.log, instead of the previous destination of $HOME/.shared.backup.log.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -5,9 +5,9 @@ SHARED_DIR=/mnt/shared
 MAC_BACKUP=/mnt/mac_backup
 _BACKUP=/mnt/_backup
 
-rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*"
-rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*"
-rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*"
-rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*"
-rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*"
-rsync -vruEthim --progress --log-file=$HOME/.shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*"
