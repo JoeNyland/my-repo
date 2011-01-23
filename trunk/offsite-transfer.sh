d0860718309bed3647f1168b38d1e4b9a95e6313
Joe Nyland <joenyland@me.com>
2011-01-23 12:54:12 +0000

    Scheduled backup jobs now report their status to /tmp/overnight-jobs.log, which is piped into an email that is scheduled to be sent every day at 09:00 to joenyland@me.com.

--- trunk/offsite-transfer.sh
+++ trunk/offsite-transfer.sh
@@ -2,22 +2,107 @@
 # Offsite Backup Transfer Script
 
 SERVERBACKUPLOCAL=/mnt/usb_backup/Backups/Server
-SERVERBACKUPREMOTE=/mnt/backup/Server
+SERVERBACKUPREMOTE=/mnt/mac_backup/Server
 
 TOOLSBACKUPLOCAL=/mnt/usb_backup/Backups/Tools
-TOOLSBACKUPREMOTE=/mnt/backup/Tools
-
-XBOXBACKUPLOCAL=/mnt/usb_backup/Backups/Xbox
-XBOXBACKUPREMOTE=/mnt/backup/Xbox
+TOOLSBACKUPREMOTE=/mnt/mac_backup/Tools
 
 SVNBACKUPLOCAL=/mnt/usb_backup/Backups/SVN
-SVNBACKUPREMOTE=/mnt/backup/SVN
+SVNBACKUPREMOTE=/mnt/mac_backup/SVN
 
-GENTOOXBACKUPLOCAL=/mnt/usb_backup/Backups/Gentoox
-GENTOOXBACKUPREMOTE=/mnt/backup/Gentoox
+if [ -f $SERVERBACKUPLOCAL/server-backup.log ]; then
+rm -rf $SERVERBACKUPREMOTE/*
+cp -v $SERVERBACKUPLOCAL/* $SERVERBACKUPREMOTE > $SERVERBACKUPREMOTE/server-backup-transfer.log
+	if [ -f $SERVERBACKUPREMOTE/server-backup.log ]; then
+	rm -rf $SERVERBACKUPLOCAL/*
+	else
+	exit
+	fi
+else
+exit
+fi
 
-rm -rf $SERVERBACKUPREMOTE/* && cp -v $SERVERBACKUPLOCAL/* $SERVERBACKUPREMOTE > $SERVERBACKUPREMOTE/server-backup-transfer.log && rm -rf $SERVERBACKUPLOCAL/*
-rm -rf $TOOLSBACKUPREMOTE/* && cp -v $TOOLSBACKUPLOCAL/* $TOOLSBACKUPREMOTE > $TOOLSBACKUPREMOTE/tools-usb-backup-transfer.log && rm -rf $TOOLSBACKUPLOCAL/*
-rm -rf $SVNBACKUPREMOTE/* && cp -v $SVNBACKUPLOCAL/* $SVNBACKUPREMOTE > $SVNBACKUPREMOTE/svn-backup-transfer.log && rm -rf $SVNBACKUPLOCAL/*
-cp -v $XBOXBACKUPLOCAL/* $XBOXBACKUPREMOTE > $XBOXBACKUPREMOTE/xbox-backup-transfer.log && rm -rf $XBOXBACKUPLOCAL/*
-cp -v $GENTOOXBACKUPLOCAL/* $GENTOOXBACKUPREMOTE > $GENTOOXBACKUPREMOTE/gentoox-backup-transfer.log && rm -rf $GENTOOXBACKUPLOCAL/*
+if [ -f $TOOLSBACKUPLOCAL/tools-usb-backup.log ]; then
+rm -rf $TOOLSBACKUPREMOTE/*
+cp -v $TOOLSBACKUPLOCAL/* $TOOLSBACKUPREMOTE > $TOOLSBACKUPREMOTE/tools-usb-backup-transfer.log
+	if [ -f $TOOLSBACKUPREMOTE/tools-usb-backup.log ]; then
+	rm -rf $TOOLSBACKUPLOCAL/*
+	else
+	exit
+	fi
+else
+exit
+fi
+
+if [ -f $SVNBACKUPLOCAL/svn-backup.log ]; then
+rm -rf $SVNBACKUPREMOTE/*
+cp -v $SVNBACKUPLOCAL/* $SVNBACKUPREMOTE > $SVNBACKUPREMOTE/svn-backup-transfer.log
+	if [ -f $SVNBACKUPREMOTE/svn-backup.log ]; then
+	rm -rf $SVNBACKUPLOCAL/*
+	else
+	exit
+	fi
+else
+exit
+fi
+
+if [ -f $SERVERBACKUPREMOTE/server-backup.log ]; then
+	if [ -f $TOOLSBACKUPREMOTE/tools-usb-backup.log ]; then
+		if [ -f $SVNBACKUPREMOTE/svn-backup.log ]; then
+	                echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
+	                echo "" >> /tmp/overnight-jobs.log
+	                echo "The offsite transfer of backup files from your server to your iMac completed successfully last night." >> /tmp/overnight-jobs.log
+	                echo "" >> /tmp/overnight-jobs.log
+	                echo "" >> /tmp/overnight-jobs.log
+	                echo "" >> /tmp/overnight-jobs.log
+
+		else
+		        echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+                        echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+                        echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+                        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+			tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+                        tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log			
+                        echo "" >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+                        echo "" >> /tmp/overnight-jobs.log
+		fi
+	else
+	        echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+               	tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+        	echo "" >> /tmp/overnight-jobs.log
+			
+	fi
+else
+	echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+        echo "" >> /tmp/overnight-jobs.log
+fi
+
--- trunk/svn-backup.sh
+++ trunk/svn-backup.sh
@@ -2,5 +2,26 @@
 # Subversion Repository Backup Script
 
 cd /mnt/usb_backup/Backups/SVN || exit
-tar cvpjf SVN-Repositories-`date +%H-%M_%d-%m-%y`.tar.bz2 /srv/svn > svn-backup.log
+tar cvpjf "SVN-Repositories-`date +%F`.tar.bz2" /mnt/shared/SVN > svn-backup.log
+
+if [ -f /mnt/usb_backup/Backups/SVN/"SVN-Repositories-`date +%F`.tar.bz2" ]; then
+                echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "The backup of your SVN repositories completed successfully last night." >> /tmp/overnight-jobs.log
+                echo "Please see below for the file name..." >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                ls -lah /mnt/usb_backup/Backups/SVN/"SVN-Repositories-`date +%F`.tar.bz2" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+        else
+                echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "The backup of your SVN repositories failed to run last night. Please verify the tar backup log below..." >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                tail /mnt/usb_backup/Backups/SVN/svn-backup.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+                echo "" >> /tmp/overnight-jobs.log
+fi
 
