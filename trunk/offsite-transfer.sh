cc4cc07d9ea73f119ab9be04a373938d8d883753
Joe Nyland <joenyland@me.com>
2011-02-10 17:48:47 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/offsite-transfer.sh
+++ trunk/offsite-transfer.sh
@@ -14,125 +14,113 @@ EMAILBACKUPLOCAL=/mnt/usb_backup/Backups/Email/MobileMe
 EMAILBACKUPREMOTE=/mnt/mac_backup/Email/MobileMe
 
 if [ -f $SERVERBACKUPLOCAL/server-backup.log ]; then
-rm -rf $SERVERBACKUPREMOTE/*
+rm -rfv $SERVERBACKUPREMOTE/*
 cp -v $SERVERBACKUPLOCAL/* $SERVERBACKUPREMOTE > $SERVERBACKUPREMOTE/server-backup-transfer.log
-	if [ -f $SERVERBACKUPREMOTE/server-backup.log ]; then
-	rm -rf $SERVERBACKUPLOCAL/*
-	else
-	fi
-else
+	elif [ -f $SERVERBACKUPREMOTE/server-backup.log ]; then
+	rm -rfv $SERVERBACKUPLOCAL/*
 fi
 
 if [ -f $TOOLSBACKUPLOCAL/tools-usb-backup.log ]; then
-rm -rf $TOOLSBACKUPREMOTE/*
+rm -rfv $TOOLSBACKUPREMOTE/*
 cp -v $TOOLSBACKUPLOCAL/* $TOOLSBACKUPREMOTE > $TOOLSBACKUPREMOTE/tools-usb-backup-transfer.log
-	if [ -f $TOOLSBACKUPREMOTE/tools-usb-backup.log ]; then
-	rm -rf $TOOLSBACKUPLOCAL/*
-	else
-	fi
-else
+	elif [ -f $TOOLSBACKUPREMOTE/tools-usb-backup.log ]; then
+	rm -rfv $TOOLSBACKUPLOCAL/*
 fi
 
 if [ -f $SVNBACKUPLOCAL/svn-backup.log ]; then
-rm -rf $SVNBACKUPREMOTE/*
+rm -rfv $SVNBACKUPREMOTE/*
 cp -v $SVNBACKUPLOCAL/* $SVNBACKUPREMOTE > $SVNBACKUPREMOTE/svn-backup-transfer.log
-	if [ -f $SVNBACKUPREMOTE/svn-backup.log ]; then
-	rm -rf $SVNBACKUPLOCAL/*
-	else
-	fi
-else
+	elif [ -f $SVNBACKUPREMOTE/svn-backup.log ]; then
+	rm -rfv $SVNBACKUPLOCAL/*
 fi
 
 if [ -f /var/log/email-backup/MobileMe_Deleted_Messages.log ]; then
-rm -rf $EMAILBACKUPREMOTE/*
+rm -rfv $EMAILBACKUPREMOTE/*
 cp -v $EMAILBACKUPLOCAL/* $EMAILBACKUPREMOTE > $EMAILBACKUPREMOTE/email-backup-transfer.log
-        if [ -f $EMAILBACKUPREMOTE/email-backup-transfer.log ]; then
-        rm -rf $EMAILBACKUPLOCAL/*
-        else
-        fi
-else
+        elif [ -f $EMAILBACKUPREMOTE/email-backup-transfer.log ]; then
+        rm -rfv $EMAILBACKUPLOCAL/*
 fi
 
 if [ -f $SERVERBACKUPREMOTE/server-backup.log ]; then
 	if [ -f $TOOLSBACKUPREMOTE/tools-usb-backup.log ]; then
 		if [ -f $SVNBACKUPREMOTE/svn-backup.log ]; then
 			if [ -f $EMAILBACKUPREMOTE/email-backup-transfer.log ]; then
-		                echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
-		                echo "" >> /tmp/overnight-jobs.log
-		                echo "The offsite transfer of backup files from your server to your iMac completed successfully last night." >> /tmp/overnight-jobs.log
-		                echo "" >> /tmp/overnight-jobs.log
-		                echo "" >> /tmp/overnight-jobs.log
-		                echo "" >> /tmp/overnight-jobs.log
+		                echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
+		                echo "" >> /var/log/overnight-jobs.log
+		                echo "The offsite transfer of backup files from your server to your iMac completed successfully last night." >> /var/log/overnight-jobs.log
+		                echo "" >> /var/log/overnight-jobs.log
+		                echo "" >> /var/log/overnight-jobs.log
+		                echo "" >> /var/log/overnight-jobs.log
 	
 			else
-			        echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
-	                        echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
-	                        echo "Please view one of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
-	                        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
-				tail $TOOLSBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
-	                        tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log			
-                                echo "" >> /tmp/overnight-jobs.log
-                                tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /tmp/overnight-jobs.log	                        
-				echo "" >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
-	                        echo "" >> /tmp/overnight-jobs.log
+			        echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
+	                        echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
+	                        echo "Please view one of the following log files for more information why this job did not compelte successfully..." >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
+	                        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
+				tail $TOOLSBACKUPREMOTE/tools-backup-transfer.log >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
+	                        tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /var/log/overnight-jobs.log			
+                                echo "" >> /var/log/overnight-jobs.log
+                                tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /var/log/overnight-jobs.log	                        
+				echo "" >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
+	                        echo "" >> /var/log/overnight-jobs.log
 			fi
 		else
-		        echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log
-	                echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log
-	                echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log
-	                tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log
-	               	tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log
-	                tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log
-                        echo "" >> /tmp/overnight-jobs.log
-                        tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /tmp/overnight-jobs.log	                        
-			echo "" >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log
-	                echo "" >> /tmp/overnight-jobs.log		
+		        echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log
+	                echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log
+	                echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log
+	                tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log
+	               	tail $SERVERBACKUPREMOTE/tools-backup-transfer.log >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log
+	                tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /var/log/overnight-jobs.log
+                        echo "" >> /var/log/overnight-jobs.log
+                        tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /var/log/overnight-jobs.log	                        
+			echo "" >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log
+	                echo "" >> /var/log/overnight-jobs.log		
 		fi
 	else
-		echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
-	        echo "" >> /tmp/overnight-jobs.log
-	        echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
-	        echo "" >> /tmp/overnight-jobs.log
-	        echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
-	        echo "" >> /tmp/overnight-jobs.log
-	        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	        echo "" >> /tmp/overnight-jobs.log
-	        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	        echo "" >> /tmp/overnight-jobs.log
-	        tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log
-                echo "" >> /tmp/overnight-jobs.log
-                tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /tmp/overnight-jobs.log	                        
-		echo "" >> /tmp/overnight-jobs.log
-                echo "" >> /tmp/overnight-jobs.log
-                echo "" >> /tmp/overnight-jobs.log
+		echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
+	        echo "" >> /var/log/overnight-jobs.log
+	        echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /var/log/overnight-jobs.log
+	        echo "" >> /var/log/overnight-jobs.log
+	        echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /var/log/overnight-jobs.log
+	        echo "" >> /var/log/overnight-jobs.log
+	        tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /var/log/overnight-jobs.log
+	        echo "" >> /var/log/overnight-jobs.log
+	        tail $SERVERBACKUPREMOTE/tools-backup-transfer.log >> /var/log/overnight-jobs.log
+	        echo "" >> /var/log/overnight-jobs.log
+	        tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /var/log/overnight-jobs.log
+                echo "" >> /var/log/overnight-jobs.log
+                tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /var/log/overnight-jobs.log	                        
+		echo "" >> /var/log/overnight-jobs.log
+                echo "" >> /var/log/overnight-jobs.log
+                echo "" >> /var/log/overnight-jobs.log
 	fi
 else
-	echo "______________________________________________________________________________________________________________________" >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
-	echo "" >> /tmp/overnight-jobs.log
+	echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	echo "At least one set of backup files failed to transfer last night from your server to your iMac." >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	echo "Please view on of the following log files for more information why this job did not compelte successfully..." >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	tail $SERVERBACKUPREMOTE/server-backup-transfer.log >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	tail $SERVERBACKUPREMOTE/tools-backup-transfer.log >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	tail $SVNBACKUPREMOTE/svn-backup-transfer.log >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	tail $EMAILBACKUPREMOTE/email-backup-transfer.log >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
+	echo "" >> /var/log/overnight-jobs.log
 fi
