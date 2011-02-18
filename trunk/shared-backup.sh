ff08a60e5b038c8e50de52ec13fdd912c5f3daa5
Joe Nyland <joenyland@me.com>
2011-02-18 20:29:34 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -13,8 +13,7 @@ rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/App
 rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
 rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
 rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SERVERBACKUPDRIVE/Backups/ $BACKUPDRIVE/Server\ Backups/ --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*"
+rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" || exit
 
 echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
 echo "" >> /var/log/overnight-jobs.log
@@ -26,7 +25,6 @@ echo "/mnt/shared/Disc Images --> \\\Backups\Blu Ray Movies" >> /var/log/overnig
 echo "/mnt/shared/Music --> \\\Backups\Blu Ray Movies" >> /var/log/overnight-jobs.log
 echo "/mnt/shared/Movies/Blu Ray Movies --> \\\Backups\Blu Ray Movies" >> /var/log/overnight-jobs.log
 echo "/mnt/shared --> Backup Drive" >> /var/log/overnight-jobs.log
-echo "/mnt/usb_backup/Backups --> Backup Drive" >> /var/log/overnight-jobs.log
 echo "" >> /var/log/overnight-jobs.log
 echo "If you require more information, please run the following command:" >> /var/log/overnight-jobs.log
 echo "cat /var/log/shared-backup.log" >> /var/log/overnight-jobs.log
