99e4006acd84b9b9007259eb4a844c2331c531a2
Joe Nyland <joenyland@me.com>
2011-03-14 10:21:05 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -7,22 +7,22 @@ _BACKUP=/mnt/_backup
 BACKUPDRIVE=/mnt/backup
 SERVERBACKUPDRIVE=/mnt/usb_backup
 
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthim --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" || exit
 
 echo "______________________________________________________________________________________________________________________" >> /var/log/overnight-jobs.log
 echo "" >> /var/log/overnight-jobs.log
 echo "The Rsync job to synchronise the following items completed successfully last night." >> /var/log/overnight-jobs.log
 echo "/mnt/shared/TV Shows --> \\s-iMac\Backups\Shared\TV Shows" >> /var/log/overnight-jobs.log
 echo "/mnt/shared/Movies --> \\s-iMac\Backups\Shared\Movies" >> /var/log/overnight-jobs.log
-echo "/mnt/shared/Applications --> \\\Backups\Blu Ray Movies" >> /var/log/overnight-jobs.log
-echo "/mnt/shared/Disc Images --> \\\Backups\Blu Ray Movies" >> /var/log/overnight-jobs.log
-echo "/mnt/shared/Music --> \\\Backups\Blu Ray Movies" >> /var/log/overnight-jobs.log
+echo "/mnt/shared/Applications --> \\\Backups\Applications" >> /var/log/overnight-jobs.log
+echo "/mnt/shared/Disc Images --> \\\Backups\Disc Images" >> /var/log/overnight-jobs.log
+echo "/mnt/shared/Music --> \\\Backups\Music" >> /var/log/overnight-jobs.log
 echo "/mnt/shared/Movies/Blu Ray Movies --> \\\Backups\Blu Ray Movies" >> /var/log/overnight-jobs.log
 echo "/mnt/shared --> Backup Drive" >> /var/log/overnight-jobs.log
 echo "" >> /var/log/overnight-jobs.log
