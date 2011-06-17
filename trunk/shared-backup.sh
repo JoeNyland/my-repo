fbee0ecd9ad6fba2c964f8e6a1be7effb117d5ab
Joe Nyland <joenyland@me.com>
2011-06-17 22:23:40 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -14,11 +14,11 @@ TOADDRESS=
 
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*" --exclude="apt-mirror" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" --exclude="apt-mirror" || exit
 
 echo "The Rsync job to synchronise the following items completed successfully last night." > /tmp/shared-backup-report.log
 echo "/mnt/shared/TV Shows --> \\s-iMac\Backups\Shared\TV Shows" >> /tmp/shared-backup-report.log
