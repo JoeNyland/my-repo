fead1d9ed3c06bc78228dbeda61c55f3253b1570
Joe Nyland <joenyland@me.com>
2011-07-14 14:18:30 +0000

    Disabled backups to iMac and DiningRoom-PC from server in shared-backup.sh Rsync script.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -12,21 +12,21 @@ SMTPPASSWORD=
 FROMADDRESS=
 TOADDRESS=
 
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*" --exclude="apt-mirror" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
+#rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/TV\ Shows $MAC_BACKUP/Shared --exclude="._*" || exit
+#rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies $MAC_BACKUP/Shared/ --exclude="Blu Ray Movies" --exclude="._*" || exit
+#rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Applications $_BACKUP/ --exclude="._*" --exclude="apt-mirror" || exit
+#rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
+#rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
+#rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" --exclude="apt-mirror" || exit
 
 echo "The Rsync job to synchronise the following items completed successfully last night." > /tmp/shared-backup-report.log
-echo "/mnt/shared/TV Shows --> \\s-iMac\Backups\Shared\TV Shows" >> /tmp/shared-backup-report.log
-echo "/mnt/shared/Movies --> \\s-iMac\Backups\Shared\Movies" >> /tmp/shared-backup-report.log
-echo "/mnt/shared/Applications --> \\\Backups\Applications" >> /tmp/shared-backup-report.log
-echo "/mnt/shared/Disc Images --> \\\Backups\Disc Images" >> /tmp/shared-backup-report.log
-echo "/mnt/shared/Music --> \\\Backups\Music" >> /tmp/shared-backup-report.log
-echo "/mnt/shared/Movies/Blu Ray Movies --> \\\Backups\Blu Ray Movies" >> /tmp/shared-backup-report.log
+#echo "/mnt/shared/TV Shows --> \\s-iMac\Backups\Shared\TV Shows" >> /tmp/shared-backup-report.log
+#echo "/mnt/shared/Movies --> \\s-iMac\Backups\Shared\Movies" >> /tmp/shared-backup-report.log
+#echo "/mnt/shared/Applications --> \\\Backups\Applications" >> /tmp/shared-backup-report.log
+#echo "/mnt/shared/Disc Images --> \\\Backups\Disc Images" >> /tmp/shared-backup-report.log
+#echo "/mnt/shared/Music --> \\\Backups\Music" >> /tmp/shared-backup-report.log
+#echo "/mnt/shared/Movies/Blu Ray Movies --> \\\Backups\Blu Ray Movies" >> /tmp/shared-backup-report.log
 echo "/mnt/shared --> Backup Drive" >> /tmp/shared-backup-report.log
 echo "" >> /tmp/shared-backup-report.log
 echo "If you require more information, please run the following command:" >> /tmp/shared-backup-report.log
