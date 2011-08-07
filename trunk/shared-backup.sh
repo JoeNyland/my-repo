afa42fd9c51bf46f4ed0fc46a93493e122f90e21
Joe Nyland <joenyland@me.com>
2011-08-07 10:14:29 +0000

    Included TimeMachine images in shared-backup.sh.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -2,6 +2,7 @@
 # Shared files backup script to backup files from /mnt/server to 's iMac
 
 SHARED_DIR=/mnt/array/Shared
+TIMEMACHINE_DIR=/mnt/array/TimeMachine
 MAC_BACKUP=/mnt/mac_backup
 _BACKUP=/mnt/_backup
 BACKUPDRIVE=/mnt/backup
@@ -18,7 +19,8 @@ TOADDRESS=
 #rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Disc\ Images $_BACKUP/ --exclude="._*" || exit
 #rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Music $_BACKUP/ --exclude="._*" || exit
 #rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
-rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" --exclude="apt-mirror" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $TIMEMACHINE_DIR/ $BACKUPDRIVE/TimeMachine/ --exclude="._*" --exclude=".AppleDB*" --exclude="lost+found" || exit
 
 echo "The Rsync job to synchronise the following items completed successfully last night." > /tmp/shared-backup-report.log
 #echo "/mnt/shared/TV Shows --> \\s-iMac\Backups\Shared\TV Shows" >> /tmp/shared-backup-report.log
