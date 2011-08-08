73615a89937f23867dca17da08dfe8d40b6f0d19
Joe Nyland <joenyland@me.com>
2011-08-08 21:03:12 +0000

    Made a few changes to shared-backup.sh after re arrangement of /mnt/array and /mnt/array/Shared.

--- trunk/shared-backup.sh
+++ trunk/shared-backup.sh
@@ -3,6 +3,8 @@
 
 SHARED_DIR=/mnt/array/Shared
 TIMEMACHINE_DIR=/mnt/array/TimeMachine
+SVN=/mnt/array/svn
+MYTHTV=/mnt/array/mythtv
 MAC_BACKUP=/mnt/mac_backup
 _BACKUP=/mnt/_backup
 BACKUPDRIVE=/mnt/backup
@@ -21,6 +23,8 @@ TOADDRESS=
 #rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/Movies/Blu\ Ray\ Movies $_BACKUP/ --exclude="._*" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SHARED_DIR/ $BACKUPDRIVE/Shared/ --exclude="._*" --exclude="Downloads/" --exclude=".AppleDB*" --exclude="lost+found" || exit
 rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $TIMEMACHINE_DIR/ $BACKUPDRIVE/TimeMachine/ --exclude="._*" --exclude=".AppleDB*" --exclude="lost+found" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $SVN/ $BACKUPDRIVE/SVN/ --exclude="._*" --exclude=".AppleDB*" || exit
+rsync -vruEthm --progress --log-file=/var/log/shared-backup.log $MYTHTV/ $BACKUPDRIVE/MythTV/ --exclude="._*" --exclude=".AppleDB*" --exclude="livetv/*"|| exit
 
 echo "The Rsync job to synchronise the following items completed successfully last night." > /tmp/shared-backup-report.log
 #echo "/mnt/shared/TV Shows --> \\s-iMac\Backups\Shared\TV Shows" >> /tmp/shared-backup-report.log
@@ -29,7 +33,10 @@ echo "The Rsync job to synchronise the following items completed successfully la
 #echo "/mnt/shared/Disc Images --> \\\Backups\Disc Images" >> /tmp/shared-backup-report.log
 #echo "/mnt/shared/Music --> \\\Backups\Music" >> /tmp/shared-backup-report.log
 #echo "/mnt/shared/Movies/Blu Ray Movies --> \\\Backups\Blu Ray Movies" >> /tmp/shared-backup-report.log
-echo "/mnt/shared --> Backup Drive" >> /tmp/shared-backup-report.log
+echo "/mnt/array/Shared --> Backup Drive" >> /tmp/shared-backup-report.log
+echo "/mnt/array/svn --> Backup Drive" >> /tmp/shared-backup-report.log
+echo "/mnt/array/TimeMachine --> Backup Drive" >> /tmp/shared-backup-report.log
+echo "/mnt/array/mythtv --> Backup Drive" >> /tmp/shared-backup-report.log
 echo "" >> /tmp/shared-backup-report.log
 echo "If you require more information, please view the following file:" >> /tmp/shared-backup-report.log
 echo "/var/log/shared-backup.log" >> /tmp/shared-backup-report.log
