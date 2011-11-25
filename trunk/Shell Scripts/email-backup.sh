78c1b391f44202b7cfe7e87c4da5f8c415ef7c15
Joe Nyland <joenyland@me.com>
2011-11-25 09:35:12 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/Shell Scripts/email-backup.sh
+++ trunk/Shell Scripts/email-backup.sh
@@ -20,13 +20,13 @@ touch /tmp/email-backup/iCloud/Deleted_Messages.mbox
 touch /tmp/email-backup/iCloud/Sent_Messages.mbox
 
 # Backup iCloud mailboxes, tar and remove source files.
-getmail --getmaildir=/home//.getmail -r icloud_Archive || exit
-getmail --getmaildir=/home//.getmail -r icloud_Trash || exit
-getmail --getmaildir=/home//.getmail -r icloud_Sent || exit
+getmail --getmaildir=/home//.getmail -r icloud_Archive || exit 1000
+getmail --getmaildir=/home//.getmail -r icloud_Trash || exit 1000
+getmail --getmaildir=/home//.getmail -r icloud_Sent || exit 1000
 
-tar cvjf /mnt/backup/Email/iCloud/Archive-`date +%F`.tar.bz2 /tmp/email-backup/iCloud/Archive.mbox
-tar cvjf /mnt/backup/Email/iCloud/Deleted_Messages-`date +%F`.tar.bz2 /tmp/email-backup/iCloud/Deleted_Messages.mbox
-tar cvjf /mnt/backup/Email/iCloud/Sent_Messages-`date +%F`.tar.bz2 /tmp/email-backup/iCloud/Sent_Messages.mbox
+tar cvjf /mnt/backup/Email/iCloud/Archive-`date +%F`.tar.bz2 /tmp/email-backup/iCloud/Archive.mbox || exit 1000
+tar cvjf /mnt/backup/Email/iCloud/Deleted_Messages-`date +%F`.tar.bz2 /tmp/email-backup/iCloud/Deleted_Messages.mbox || exit 1000
+tar cvjf /mnt/backup/Email/iCloud/Sent_Messages-`date +%F`.tar.bz2 /tmp/email-backup/iCloud/Sent_Messages.mbox || exit 1000
 
 rm -rfv /tmp/email-backup/
 
@@ -39,11 +39,13 @@ if [ -f /mnt/backup/Email/iCloud/Archive-`date +%F`.tar.bz2 ]; then
 		echo "" >> /tmp/email-backup-report.log
 		ls -lah /mnt/backup/Email/iCloud/Archive-`date +%F`.tar.bz2 >> /tmp/email-backup-report.log
 		sendemail -f $FROMADDRESS -t $TOADDRESS -s $SMTPSERVER -o username=$SMTPUSERNAME -o password=$SMTPPASSWORD -u "'Archive' on iCloud backup success for " < /tmp/email-backup-report.log
+		export SUCCESS1=1
 	else
 		echo "The backup of the 'Archive' folder on iCloud failed to run last night. Please verify IMAP backup log below..." > /tmp/email-backup-report.log
         	echo "" >> /tmp/email-backup-report.log
 		tail /var/log/email-backup/iCloud_Archive.log >> /tmp/email-backup-report.log
 		sendemail -f $FROMADDRESS -t $TOADDRESS -s $SMTPSERVER -o username=$SMTPUSERNAME -o password=$SMTPPASSWORD -u "'Archive' on iCloud backup failure for " < /tmp/email-backup-report.log
+		export SUCCESS1=0
 fi
 
 if [ -f /mnt/backup/Email/iCloud/Deleted_Messages-`date +%F`.tar.bz2 ]; then 
@@ -52,11 +54,13 @@ if [ -f /mnt/backup/Email/iCloud/Deleted_Messages-`date +%F`.tar.bz2 ]; then
 		echo "" >> /tmp/email-backup-report.log
 		ls -lah /mnt/backup/Email/iCloud/Deleted_Messages-`date +%F`.tar.bz2 >> /tmp/email-backup-report.log
 		sendemail -f $FROMADDRESS -t $TOADDRESS -s $SMTPSERVER -o username=$SMTPUSERNAME -o password=$SMTPPASSWORD -u "'Deleted Messages' on iCloud backup success for " < /tmp/email-backup-report.log
+		export SUCCESS2=1
 	else
 		echo "The backup of the 'Deleted Messages' folder on iCloud failed to run last night. Please verify IMAP backup log below..." > /tmp/email-backup-report.log
         	echo "" >> /tmp/email-backup-report.log
 		tail /var/log/email-backup/iCloud_Deleted_Messages.log >> /tmp/email-backup-report.log
 		sendemail -f $FROMADDRESS -t $TOADDRESS -s $SMTPSERVER -o username=$SMTPUSERNAME -o password=$SMTPPASSWORD -u "'Deleted Messages' on iCloud backup failure for " < /tmp/email-backup-report.log
+		export SUCCESS2=0
 fi
 
 if [ -f /mnt/backup/Email/iCloud/Sent_Messages-`date +%F`.tar.bz2 ]; then 
@@ -65,11 +69,20 @@ if [ -f /mnt/backup/Email/iCloud/Sent_Messages-`date +%F`.tar.bz2 ]; then
 		echo "" >> /tmp/email-backup-report.log
 		ls -lah /mnt/backup/Email/iCloud/Sent_Messages-`date +%F`.tar.bz2 >> /tmp/email-backup-report.log
 		sendemail -f $FROMADDRESS -t $TOADDRESS -s $SMTPSERVER -o username=$SMTPUSERNAME -o password=$SMTPPASSWORD -u "'Sent Messages' on iCloud backup success for " < /tmp/email-backup-report.log
+		export SUCCESS3=1
 	else
 		echo "The backup of the 'Sent Messages' folder on iCloud failed to run last night. Please verify IMAP backup log below..." > /tmp/email-backup-report.log
         	echo "" >> /tmp/email-backup-report.log
 		tail /var/log/email-backup/iCloud_Sent_Messages.log >> /tmp/email-backup-report.log
 		sendemail -f $FROMADDRESS -t $TOADDRESS -s $SMTPSERVER -o username=$SMTPUSERNAME -o password=$SMTPPASSWORD -u "'Sent Messages' on iCloud backup failure for " < /tmp/email-backup-report.log
+		export SUCCESS3=0
 fi
 
 rm -rfv /tmp/email-backup-report.log
+
+# Clear up old backups
+if [[ $SUCCESS1 == 1 ]] && [[ $SUCCESS2 == 1 ]] && [[ $SUCCESS3 == 1 ]]; then
+	find /mnt/backup/`hostname -s`/ -iname `hostname -s`*.iso -daystart -mtime +$AGE -exec rm -fv {} \;
+else
+	echo $SUCCESS1 $SUCCESS2 $SUCCESS3;
+fi
