f6a3cbfb44c11e0da1f510af3bd031fbb8bc8b3e
Joe Nyland <joenyland@me.com>
2011-11-26 20:07:18 +0000

    This commit did not have an SVN commit message when it was migrated.

---
+++ trunk/Shell Scripts/transmission-email-notify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+/usr/bin/sendemail -f "Transmission@" -t "" -s "" -o username= -o password= -u "Transmission completed download: $TR_TORRENT_NAME" <<EOF
+Transmission has completed downloading $TR_TORRENT_NAME.
+
+The downloaded files can be found here: $TR_TORRENT_DIR
+
+
+
+($TR_APP_VERSION)
+EOF
