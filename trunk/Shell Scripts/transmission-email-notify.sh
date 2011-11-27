5ba20d747b249284bfeb270c4efdf9587c97fe29
Joe Nyland <joenyland@me.com>
2011-11-27 14:03:19 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/Shell Scripts/transmission-email-notify.sh
+++ trunk/Shell Scripts/transmission-email-notify.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 /usr/bin/sendemail -f "Transmission@" -t "" -s "" -o username= -o password= -u "Transmission completed download: $TR_TORRENT_NAME" <<EOF
-Transmission has completed downloading $TR_TORRENT_NAME.
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 
-The downloaded files can be found here: $TR_TORRENT_DIR
+<html>
 
+<body>
+    <font face="Lucida Grande" color="#000000">
+        <h2>Transmission has completed downloading "$TR_TORRENT_NAME"</h2>
+    </font>
 
+    <font face="Lucida Grande" color="#000000" size="3">
+        <p>The downloaded files can be found here: $TR_TORRENT_DIR</p>
+    </font>
+    <font face="Lucida Grande" color="#C0C0C0" size="1">
+        </br>
+        </br>
+        </br>
+        <a href="http://www.transmissionbt.com"><img src="http://www.transmissionbt.com/images/transmission.png" alt="Transmission" height="16px" width="155px"/><img src="http://www.transmissionbt.com/images/transmission-74x74.png" alt="Transmission" height="24px" width="24px"/></a>
+        </br>
+        (v$TR_APP_VERSION)
+    </font>
+</body>
 
-
-Transmission ($TR_APP_VERSION)
+</html>
 EOF
