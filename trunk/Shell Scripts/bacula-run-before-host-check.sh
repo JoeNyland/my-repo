352e794aa2ccbe7fc1fd028d27cc36cf6b9ab321
Joe Nyland <joenyland@me.com>
2011-12-30 21:13:40 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/Shell Scripts/bacula-run-before-host-check.sh
+++ trunk/Shell Scripts/bacula-run-before-host-check.sh
@@ -2,11 +2,10 @@
 
 # Script to check whether or not Gentoox is running on my Xbox, in order to decide whether a Bacula backup should be attempted.
 
-HOSTNAME=$1				# Specify the host to test (DNS hostname only).
-DOMAIN     # Specify the domain name of which the host is part of.
+HOSTNAME=$1		# Specify the host to test (DNS hostname only).
 PORT=9102               # Specify the TCP port to check (Here we check port 9102, which is the default port for the Bacula File Daemon).
 
-if nc -zv -w30 $HOST.$DOMAIN $PORT                
+if nc -zv -w30 $HOSTNAME $PORT
 then
         echo "Port is open";
         exit 0;
