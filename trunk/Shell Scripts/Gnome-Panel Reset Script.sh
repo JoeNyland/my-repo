2d01346a94f8de2c43883ce3e201ab79f8a52b83
Joe Nyland <joenyland@me.com>
2012-01-01 22:33:34 +0000

    Removed the need to log the user out then back in again for the restored gnome-panel to be used.

--- trunk/Shell Scripts/Gnome-Panel Reset Script.sh
+++ trunk/Shell Scripts/Gnome-Panel Reset Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 
-echo Welcome to the BarryCent gnome-panel restoration script.
+echo Welcome to the MasterRoot gnome-panel restoration script.
 echo 
 echo This script will restore the gnome-panel to the default settings.
 echo 
@@ -13,17 +13,15 @@ rm -rfv ~/.gconf/apps/panel/*
 cp -rfv ~/.gconf/apps/.panel-backup/* ~/.gconf/apps/panel
 clear
 echo 
-echo Restore Complete. This script will Log you out, then you will need to Log back
-echo in to apply changes.
+echo Restore Complete. This script will restart gnome-panel to apply the changes.
 echo 
 echo -n Press return to continue.
 read ans
+killall gnome-panel
 clear
-echo 
-echo You will now be logged out. Please save all open Documents, and close all
-echo running programs, to avoid data loss.
-echo 
-echo -n Press return when you are ready.
-read ans
-gnome-session-save --kill --silent
+sleep 3
+echo Script completed successfully!
+sleep 1
 exit
+
+
