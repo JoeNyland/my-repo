058571ea9835f34424a1dfbdaad9f3bbe1e21568
Joe Nyland <joenyland@me.com>
2012-01-16 22:38:21 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/Shell Scripts/sync-portage-mirror.sh
+++ trunk/Shell Scripts/sync-portage-mirror.sh
@@ -6,3 +6,11 @@ PUBLICMIRROR=rsync://rsync.uk.gentoo.org
 LOCALTREE=/mnt/array/portage-mirror
 
 rsync -hav $PUBLICMIRROR/gentoo-portage $LOCALTREE
+
+if [ $? -eq 0 ]; then
+	#rsync completed sucessfully
+	logger -ist portage-sync "The local portage mirror was successfully sync'ed with the public mirror: $PUBLICMIRROR ";
+	else
+	#rsync completed with errors
+	logger -ist portage-sync "The portage-sync script completed with errors. Please review any error messages which are displayed when the script is run manually.";
+fi
