579df53e8232318d64c51dc68adeb0fcd7068bce
Joe Nyland <joenyland@me.com>
2012-01-16 18:55:12 +0000

    This commit did not have an SVN commit message when it was migrated.

--- trunk/Shell Scripts/external-repo-sync.sh
+++ trunk/Shell Scripts/external-repo-sync.sh
@@ -5,10 +5,13 @@
 
 # NOTES
 # Copy CVS code from SF Rsync:
-#	rsync -av rsync://xbox-linux.cvs.sourceforge.net/cvsroot/xbox-linux/* xbox-linux.rsync/
+rsync -av rsync://xbox-linux.cvs.sourceforge.net/cvsroot/xbox-linux/* /tmp/xbox-linux.rsync/
+
 # Convert CVS repo to SVN dump:
-#	cvs2svn --encoding=utf_8 --fallback-encoding=latin_1 --keep-trivial-imports --cvs-revnums --dumpfile=xbox-linux.svn.dmp xbox-linux.rsync/
+cvs2svn --encoding=utf_8 --fallback-encoding=latin_1 --keep-trivial-imports --cvs-revnums --dumpfile=/tmp/xbox-linux.svn.dmp /tmp/xbox-linux.rsync/
+
 # Load SVN dump in to SVN repo:
-#	svnadmin load /mnt/array/svn/xbox/xbox-linux/ < xbox-linux.svn.dmp
-# Remove temp file:
-#	rm -rfv xbox-linux.rsync/ xbox-linux.svn.dmp
+svnadmin load /mnt/array/xbox/xbox-linux/ < xbox-linux.svn.dmp
+
+Remove temp file:
+rm -rfv /tmp/xbox-linux.rsync/ /tmp/xbox-linux.svn.dmp
