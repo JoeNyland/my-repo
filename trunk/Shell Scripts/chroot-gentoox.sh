f54e5ad94ba6085dffee8a0abb4fa1764ef7932d
Joe Nyland <joenyland@me.com>
2012-01-24 16:56:35 +0000

    Work on chroot-gentoox.sh.

--- trunk/Shell Scripts/chroot-gentoox.sh
+++ trunk/Shell Scripts/chroot-gentoox.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
 # Script to automate chrooting into Gentoox rootfs from another Linux PC.
 # Created by Joe Nyland on 17/01/2012
 
-ROOTFS=
+ROOTFS=$1
+MOUNT=$2
+
+##########################################################################
+#	* Welcome to the MasterRoot24 script to provide a chroot environment #
+#	for Gentoox rootfs file.											 #
+#																		 #
+#	* You will be prompted for the necessary details below.				 #
+#																		 #
+##########################################################################
