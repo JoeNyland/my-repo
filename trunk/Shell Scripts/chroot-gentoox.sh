9af9b09fb5c5324c4d6862b2e91b718e606bc54c
Joe Nyland <joenyland@me.com>
2012-01-24 17:00:40 +0000

    Work on chroot-gentoox.sh.

--- trunk/Shell Scripts/chroot-gentoox.sh
+++ trunk/Shell Scripts/chroot-gentoox.sh
@@ -6,9 +6,11 @@ ROOTFS=$1
 MOUNT=$2
 
 ##########################################################################
-#	* Welcome to the MasterRoot24 script to provide a chroot environment #
-#	for Gentoox rootfs file.											 #
+# 																		 #
+# * Welcome to the MasterRoot24 script to provide a chroot environment 	 #
+#   for Gentoox rootfs file.											 #
 #																		 #
-#	* You will be prompted for the necessary details below.				 #
+# * You will be prompted for the necessary details below.				 #
 #																		 #
 ##########################################################################
+
