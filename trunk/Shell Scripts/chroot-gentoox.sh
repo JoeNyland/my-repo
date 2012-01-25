9c7df906a6728d912a404b606887ff6dc37586d2
Joe Nyland <joenyland@me.com>
2012-01-25 13:59:07 +0000

    Work on chroot-gentoox.sh

--- trunk/Shell Scripts/chroot-gentoox.sh
+++ trunk/Shell Scripts/chroot-gentoox.sh
@@ -5,15 +5,27 @@
 ROOTFS=$1
 MOUNT=$2
 
-echo <<EOMOTD
-##########################################################################
-# 									 #
-# * Welcome to the MasterRoot24 script to provide a chroot environment 	 #
-#   for Gentoox rootfs file.					         #
-#									 #
-# * You will be prompted for the necessary details below.		 #
-#									 #
-##########################################################################
-EOMOTD
+SCRIPTNAME=`basename $0`
 
+cat <<EOWELC
+##########################################################################
+#                                                                        #
+# * Welcome to the MasterRoot24 script to provide a chroot environment   #
+#   for Gentoox rootfs file.                                             #
+#                                                                        #
+# * You will be prompted for the necessary details below.                #
+#                                                                        #
+##########################################################################
+EOWELC
+
+if [ $1 -f ]; then
+	echo "You have selected $1 as the Gentoox rootfs to chroot into.";
+else
+	echo "You have selected an invalid Gentoox rootfs file to chroot into.";
+	echo "Please re-run the script, with the following syntax:";
+	echo ;
+cat <<EOSYNTAX
+$SCRIPTNAME [Gentoox_rootfs] [mount_point]
+EOSYNTAX
+fi
 
