d58177d390ed9ed26fe71ec16509ff293abebdff
Joe Nyland <joenyland@me.com>
2012-02-03 23:48:48 +0000

    Fixed bug in mysql-backup.sh script, where unexpected behaviour occurred, when running the script with the same arguments over and over again.

--- trunk/Shell Scripts/Bacula Scripts/mysql-backup.sh
+++ trunk/Shell Scripts/Bacula Scripts/mysql-backup.sh
@@ -3,10 +3,6 @@
 # Directory to store backups in
 DST=/var/backups/mysql
 
-# The MySQL username and password
-DBUSER=$1
-DBPASS=$2
-
 # A regex, passed to egrep -v, for which databases to ignore
 #IGNREG='^snort$'
 IGNREG='information_schema'
@@ -14,13 +10,20 @@ IGNREG='information_schema'
 SCRIPTNAME=`basename $0`
 HOST=`hostname -s`
 
-if [[ "$1" -eq "cleanup" ]] && [[ -d ${DST} ]]; then
-	echo "Cleaning up files.";
-	if rm -rf $DST; then
-	echo "Removed temporary files from ${DST}";
+if [[ "$1" == "cleanup" ]]; then
+	if [[ -d ${DST} ]]; then
+		echo "Cleaning up files.";
+		if rm -rf $DST; then
+			echo "Removed temporary files from ${DST}";
+		fi
+	else
+		echo "No files to be cleaned.";
 	fi
 	exit 0;
 fi
+
+DBUSER=$1
+DBPASS=$2
 
 if [ -z "$DBUSER" -o -z "$DBPASS" ]; then
 	echo "[ERROR]";
@@ -33,22 +36,29 @@ fi
 
 mkdir -p ${DST}
 
-
 if [ -z "$HOST" ]; then
 	echo "Cannot determine system hostname.";
 	exit 1001;
 fi
 
-for db in $(echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} | egrep -v ${IGNREG}) ; do
-        echo "Backing up MySQL database ${db} from $HOST... "
-        if mysqldump --opt -u ${DBUSER} -p${DBPASS} ${db} | bzip2 -c > ${DST}/${HOST}_${db}.sql.bz2
-        then
-        	echo "Completed backup of MySQL database ${db} from $HOST.";
-        else
-        	echo "[ERROR]";
-        	echo "Backup of MySQL database ${db} from $HOST FAILED.";
-        	exit 1003;
-        fi
-done
-echo "Completed backing up MySQL databases from $HOST.";
+if echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} >/dev/null
+then
+	for db in $(echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} | egrep -v ${IGNREG}) ; do
+			echo "Backing up MySQL database ${db} from $HOST... "
+			if mysqldump --opt -u ${DBUSER} -p${DBPASS} ${db} | bzip2 -c > ${DST}/${HOST}_${db}.sql.bz2
+			then
+				echo "Completed backup of MySQL database ${db} from $HOST.";
+			else
+				echo "[ERROR]";
+				echo "Backup of MySQL database ${db} from $HOST FAILED.";
+				exit 1003;
+			fi
+	done
+else
+	echo "[ERROR]";
+	echo "There seems to have been an error dumping the database(s).";
+	echo "Please review the logged messages above.";
+	exit 1004;
+fi
+
 exit 0
