7da26b68733ed2082dd05f5d770f5497f25ab67d
Joe Nyland <joenyland@me.com>
2012-02-15 23:44:12 +0000

    Wrote an incremental back routine into mysql-backup.sh script.

--- trunk/Shell Scripts/Bacula Scripts/client-scripts/mysql-backup.sh
+++ trunk/Shell Scripts/Bacula Scripts/client-scripts/mysql-backup.sh
@@ -3,6 +3,11 @@
 # Directory to store backups in
 DST=/var/backups/mysql
 
+# MySQL Binary logging configuration
+MYSQLCONF=/etc/mysql/my.cnf
+BINLOGDIR=/var/log/mysql
+BINLOGPREFIX=mysql-bin
+
 # A regex, passed to egrep -v, for which databases to ignore
 #IGNREG='^snort$'
 IGNREG='information_schema'
@@ -22,13 +27,14 @@ if [[ "$1" == "cleanup" ]]; then
 	exit 0;
 fi
 
-DBUSER=$1
-DBPASS=$2
+LEVEL=$1
+DBUSER=$2
+DBPASS=$3
 
 if [ -z "$DBUSER" -o -z "$DBPASS" ]; then
 	echo "[ERROR]";
 	echo "You have not provided the required information to the script.";
-	echo "Please use the following syntax: $SCRIPTNAME [MySQL_User] [MySQL_Password]";
+	echo "Please use the following syntax: $SCRIPTNAME [MySQL_User] [MySQL_Password] [Level]";
 	echo "Or:";
 	echo "$SCRIPTNAME cleanup";
 	exit 1000;
@@ -41,24 +47,53 @@ if [ -z "$HOST" ]; then
 	exit 1001;
 fi
 
-if echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} >/dev/null
-then
-	for db in $(echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} | egrep -v ${IGNREG}) ; do
-			echo "Backing up MySQL database ${db} from $HOST... "
-			if mysqldump --opt -u ${DBUSER} -p${DBPASS} ${db} | bzip2 -c > ${DST}/${HOST}_${db}.sql.bz2
-			then
-				echo "Completed backup of MySQL database ${db} from $HOST.";
-			else
-				echo "[ERROR]";
-				echo "Backup of MySQL database ${db} from $HOST FAILED.";
-				exit 1003;
-			fi
-	done
-else
-	echo "[ERROR]";
-	echo "There seems to have been an error dumping the database(s).";
-	echo "Please review the logged messages above.";
-	exit 1004;
-fi
+case $LEVEL in
+Full|Differential)
+	# FULL BACKUP (will also run the same backup for a differential level job.)
+	# If the supplied credentials are vaild, then perform a FULL database dump to ${DST}/${HOST}_${db}.sql.bz2
+	if echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} >/dev/null
+	then
+		for db in $(echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} | egrep -v ${IGNREG}) ; do
+				echo "Backing up MySQL database ${db} from $HOST... "
+				if mysqldump --single-transaction --opt -u ${DBUSER} -p${DBPASS} ${db} | bzip2 -c > ${DST}/${HOST}_${db}.sql.bz2
+				then
+					echo "Completed backup of MySQL database ${db} from $HOST.";
+				else
+					echo "[ERROR]";
+					echo "Backup of MySQL database ${db} from $HOST FAILED.";
+					exit 1003;
+				fi
+		done
+		mysqladmin flush-logs -u ${DBUSER} -p${DBPASS}; # Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
+	else
+		echo "[ERROR]";
+		echo "There seems to have been an error dumping the database(s).";
+		echo "Please review the logged messages above.";
+		exit 1004;
+	fi;;
+Incremental)
+	# INCREMENTAL BACKUP
+	# If binary transaction logging is enabled in ${MYSQLCONF}, then the script will copy the transaction logs from ${BINLOGDIR} to ${DST}, ready to be backed up.
+	echo "Incremental MySQL backup selected for ${HOST}";
+	if grep "log_bin" ${MYSQLCONF} | egrep -v '^(#|$)' | grep "${BINLOGDIR}/${BINLOGPREFIX}" # If this returns 0, then binary logging appears to be enabled for MySQL.
+	then
+		echo "Backing up transaction logs for MySQL databases on ${HOST} to ${DST}/"
+		mysqladmin flush-logs -u ${DBUSER} -p${DBPASS}; # Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
+		cp -b ${BINLOGDIR}/${BINLOGPREFIX}* ${DST}/
+		echo "Completed backup of MySQL databases on $HOST.";
+	else
+		echo "[ERROR]";
+		echo "MySQL binary transaction logging does not appear to have been enabled in ${MYSQLCONF}.";
+		echo "You must enable binary transaction logging in MySQL for incremental backups to work.";
+		exit 1006;
+	fi;;
+*)
+	echo "[ERROR]"
+	echo "You have not specified the backup level variable in the client-run-before-job resource definition."
+	echo "Please use the following syntax: $SCRIPTNAME [MySQL_User] [MySQL_Password] [Level]";
+	echo "Or:";
+	echo "$SCRIPTNAME cleanup";
+	exit 1005;;
+esac
 
 exit 0
