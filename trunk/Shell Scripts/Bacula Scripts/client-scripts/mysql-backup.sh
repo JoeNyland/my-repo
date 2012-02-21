6c38f2b3074b335b065eec452b6e9f6d60d2f3ab
Joe Nyland <joenyland@me.com>
2012-02-21 22:56:07 +0000

    Made some changes to mysql-backup.sh script for incremental backups.

--- trunk/Shell Scripts/Bacula Scripts/client-scripts/mysql-backup.sh
+++ trunk/Shell Scripts/Bacula Scripts/client-scripts/mysql-backup.sh
@@ -24,17 +24,8 @@ if [[ "$4" == "cleanup" ]]; then
 		echo "Cleaning up files.";
 		if rm -rf $DST; then
 			echo "Removed temporary files from ${DST}";
-			if [[ "$LEVEL" == "Full" ]]; then
-				echo "Performed a full backup of the databases on ${HOST}, so we can truncate the transaction logs.";
-				if echo "RESET MASTER" | mysql -u ${DBUSER} -p${DBPASS}
-					then
-						echo "Successfully truncated the transaction logs.";
-					else
-						echo "[WARNING]";
-						echo "FAILED to truncate transaction log files from ${BINLOGDIR}";
-						echo "As this is not a fatal error, the backup will proceed";
-				fi
-			fi
+		  else
+		  echo "Failed to remove temporary files from ${DST}";
 		fi
 	else
 		echo "No files to be cleaned.";
@@ -64,9 +55,9 @@ Full|Differential)
 	# If the supplied credentials are vaild, then perform a FULL database dump to ${DST}/${HOST}_${db}.sql.bz2
 	if echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} >/dev/null
 	then
+		echo "Performing full backup of MySQL databases from $HOST... "
 		for db in $(echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} | egrep -v ${IGNREG}) ; do
-				echo "Performing full backup of MySQL database ${db} from $HOST... "
-				if mysqldump --single-transaction --opt -u ${DBUSER} -p${DBPASS} ${db} > ${DST}/${HOST}_${db}.sql.dmp
+				if mysqldump --single-transaction --delete-master-logs --flush-logs --opt -u ${DBUSER} -p${DBPASS} ${db} > ${DST}/${HOST}_${db}.sql.dmp
 				then
 					echo "Completed full backup of MySQL database ${db} from $HOST.";
 				else
@@ -75,7 +66,6 @@ Full|Differential)
 					exit 1003;
 				fi
 		done
-		mysqladmin flush-logs -u ${DBUSER} -p${DBPASS}; # Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
 	else
 		echo "[ERROR]";
 		echo "There seems to have been an error backing up the database(s).";
