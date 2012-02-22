2972f0a415ac27dcc5ac51f49f2b713f77ba22ad
Joe Nyland <joenyland@me.com>
2012-02-22 11:07:20 +0000

    More work on incremental backups of MySQL in mysql-backup.sh.

--- trunk/Shell Scripts/Bacula Scripts/client-scripts/mysql-backup.sh
+++ trunk/Shell Scripts/Bacula Scripts/client-scripts/mysql-backup.sh
@@ -8,16 +8,15 @@ MYSQLCONF=/etc/mysql/my.cnf
 BINLOGDIR=/var/log/mysql
 BINLOGPREFIX=mysql-bin
 
-# A regex, passed to egrep -v, for which databases to ignore
-#IGNREG='^snort$'
-IGNREG='information_schema'
-
 SCRIPTNAME=`basename $0`
 HOST=`hostname -s`
 
 LEVEL=$1
 DBUSER=$2
 DBPASS=$3
+
+DATE=`date +%d%m%Y`
+TIME=`date +%H%M`
 
 if [[ "$4" == "cleanup" ]]; then
 	if [[ -d ${DST} ]]; then
@@ -56,16 +55,14 @@ Full|Differential)
 	if echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} >/dev/null
 	then
 		echo "Performing full backup of MySQL databases from $HOST... "
-		for db in $(echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} | egrep -v ${IGNREG}) ; do
-				if mysqldump --single-transaction --delete-master-logs --flush-logs --opt -u ${DBUSER} -p${DBPASS} ${db} > ${DST}/${HOST}_${db}.sql.dmp
+				if mysqldump --all-databases --single-transaction --delete-master-logs --flush-logs --master-data=2 --opt -u ${DBUSER} -p${DBPASS} > ${DST}/${HOST}_${DATE}_${TIME}.sql.dmp
 				then
-					echo "Completed full backup of MySQL database ${db} from $HOST.";
+					echo "Completed full backup of MySQL databases from $HOST.";
 				else
 					echo "[ERROR]";
-					echo "Full backup of MySQL database ${db} from $HOST FAILED.";
+					echo "Full backup of MySQL databases from $HOST FAILED.";
 					exit 1003;
 				fi
-		done
 	else
 		echo "[ERROR]";
 		echo "There seems to have been an error backing up the database(s).";
@@ -78,9 +75,16 @@ Incremental)
 	echo "Incremental MySQL backup selected for ${HOST}";
 	if grep "log_bin" ${MYSQLCONF} | egrep -v '^(#|$)' | grep "${BINLOGDIR}/${BINLOGPREFIX}" > /dev/null # If this returns 0, then binary logging appears to be enabled for MySQL.
 	then
-		echo "Flushing transaction logs for MySQL databases on ${HOST} to prepare for backup."
-		mysqladmin flush-logs -u ${DBUSER} -p${DBPASS}; # Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
-		echo "MySQL binary transaction logs for databases on $HOST have been flushed and are ready to be backed up.";
+		if echo 'show databases;' | mysql -s -u ${DBUSER} -p${DBPASS} >/dev/null
+			then
+				echo "Flushing transaction logs for MySQL databases on ${HOST} to prepare for backup."
+				mysqladmin flush-logs -u ${DBUSER} -p${DBPASS}; # Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
+				echo "MySQL binary transaction logs for databases on $HOST have been flushed and are ready to be backed up.";
+			else
+		echo "[ERROR]";
+		echo "There seems to have been an error backing up the database(s).";
+		echo "Please review the logged messages above.";
+		exit 1007;
 	else
 		echo "[ERROR]";
 		echo "MySQL binary transaction logging does not appear to have been enabled in ${MYSQLCONF}.";
