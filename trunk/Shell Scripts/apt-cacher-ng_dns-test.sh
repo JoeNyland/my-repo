1c7fccfb1634a4217e2ef800964a7a24427b032c
Joe Nyland <joenyland@me.com>
2012-04-30 14:12:01 +0000

    Wrote a script to wrap around apt-cacher-ng's maintenance script that runs under cron.daily.

---
+++ trunk/Shell Scripts/apt-cacher-ng_dns-test.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+LOG=${HOME}/apt-cacher-ng_dns-test.log
+APT-CACHER-NG_MAINT=/etc/cron.daily/apt-cacher-ng
+
+QUERY1="www.google.co.uk"
+QUERY2="repo.yaffas.org"
+QUERY3="packages.vmware.com"
+QUERY4="archive.canonical.com"
+
+# Test script setup to run some DNS queries around the apt-cacher-ng maintenance script to help diagnose DNS issues when this is run from cron.
+echo "apt-cacher-ng Diagnostic script started `date %c`" >> ${LOG}
+
+# First DNS query (before apt-cacher-ng maintenance is run)
+echo "Starting preliminary DNS query checks." >> ${LOG}
+dig @ ${QUERY1} any >> ${LOG}
+dig @ ${QUERY2} any >> ${LOG}
+dig @ ${QUERY3} any >> ${LOG}
+dig @ ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+
+echo "Querying Web..." >> ${LOG}
+dig @ ${QUERY1} any >> ${LOG}
+dig @ ${QUERY2} any >> ${LOG}
+dig @ ${QUERY3} any >> ${LOG}
+dig @ ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+
+echo "Querying ..." >> ${LOG}
+dig @ ${QUERY1} any >> ${LOG}
+dig @ ${QUERY2} any >> ${LOG}
+dig @ ${QUERY3} any >> ${LOG}
+dig @ ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+
+echo "Querying Google OpenDNS..." >> ${LOG}
+dig @8.8.8.8 ${QUERY1} any >> ${LOG}
+dig @8.8.8.8 ${QUERY2} any >> ${LOG}
+dig @8.8.8.8 ${QUERY3} any >> ${LOG}
+dig @8.8.8.8 ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+# End first query
+
+
+# Call the apt-cacher-ng maintenance job
+echo "Calling ${APT-CACHER-NG_MAINT}..." >> ${LOG}
+bash ${APT-CACHER-NG_MAINT} >> ${LOG}
+
+
+# Second DNS query (after apt-cacher-ng maintenance is run)
+ echo "Starting final DNS query checks." >> ${LOG}
+dig @ ${QUERY1} any >> ${LOG}
+dig @ ${QUERY2} any >> ${LOG}
+dig @ ${QUERY3} any >> ${LOG}
+dig @ ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+
+echo "Querying Web..." >> ${LOG}
+dig @ ${QUERY1} any >> ${LOG}
+dig @ ${QUERY2} any >> ${LOG}
+dig @ ${QUERY3} any >> ${LOG}
+dig @ ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+
+echo "Querying ..." >> ${LOG}
+dig @ ${QUERY1} any >> ${LOG}
+dig @ ${QUERY2} any >> ${LOG}
+dig @ ${QUERY3} any >> ${LOG}
+dig @ ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+
+echo "Querying Google OpenDNS..." >> ${LOG}
+dig @8.8.8.8 ${QUERY1} any >> ${LOG}
+dig @8.8.8.8 ${QUERY2} any >> ${LOG}
+dig @8.8.8.8 ${QUERY3} any >> ${LOG}
+dig @8.8.8.8 ${QUERY4} any >> ${LOG}
+echo "" >> ${LOG}
+# End second query
+
+echo "apt-cacher-ng Diagnostic script finished `date %c`" >> ${LOG}
