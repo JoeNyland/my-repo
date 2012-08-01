9bbb307300ab6454298b96ccb10f5c41d95d0f11
Joe Nyland <joenyland@me.com>
2012-08-01 21:01:20 +0000

    Created sync-temp-mirror.sh script to sync shared data on FileServer1 to /mnt/backup1.

--- Scripts/trunk/Shell Scripts/sync-temp-mirror.sh
+++ Scripts/trunk/Shell Scripts/sync-temp-mirror.sh
@@ -3,7 +3,7 @@
 # This script was written to be setup as a cron job to be run every night whilst maintaining a mirror of /mnt/array on /mnt/backup1 on 
 
 # Rsync /mnt/array/shared, deleting files on the mirror is they have been deleted in the live data location.
-rsync -rtphn --del --force --progress /mnt/array/shared/ /mnt/backup1/restore/mnt/array/shared/
+rsync -rtph --del --force --progress /mnt/array/shared/ /mnt/backup1/restore/mnt/array/shared/
 
 # Rsync /mnt/array/svn, deleting files on the mirror is they have been deleted in the live data location.
-rsync -rtphn --del --force --progress /mnt/array/svn/ /mnt/backup1/restore/mnt/array/svn/
+rsync -rtph --del --force --progress /mnt/array/svn/ /mnt/backup1/restore/mnt/array/svn/
