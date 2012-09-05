b47501e50c32946b45c4c16cd6e3abe93f480733
Joe Nyland <joenyland@me.com>
2012-09-05 21:16:47 +0000

    Some work on issue #22.

---
+++ Scripts/trunk/SQL Scripts/Secure MySQL passwords.sql
@@ -0,0 +1,20 @@
+-- TestWeb
+SET PASSWORD FOR 'wordpress'@'localhost' = PASSWORD('XaTruwAW5aVu4way');
+SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
+SET PASSWORD FOR 'trac'@'localhost' = PASSWORD('pR4JASwapudrUc39');
+SET PASSWORD FOR 'www-data'@'localhost' = PASSWORD('rutrufabRetHa9aT');
+
+-- TestMail
+SET PASSWORD FOR 'dbmail'@'localhost' = PASSWORD('');
+SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
+SET PASSWORD FOR 'roundcube'@'localhost' = PASSWORD('');
+SET PASSWORD FOR 'postfix'@'localhost' = PASSWORD('');
+
+-- 
+SET PASSWORD FOR ''@'localhost' = PASSWORD('');
+SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
+SET PASSWORD FOR ''@'localhost' = PASSWORD('');
+SET PASSWORD FOR ''@'localhost' = PASSWORD('');
+
+-- Test
+SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
