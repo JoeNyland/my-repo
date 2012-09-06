6dd2e044159156abd646b329596de89202cbff1d
Joe Nyland <joenyland@me.com>
2012-09-06 06:18:46 +0000

    Some work on issue #22.

--- Scripts/trunk/SQL Scripts/Secure MySQL passwords.sql
+++ Scripts/trunk/SQL Scripts/Secure MySQL passwords.sql
@@ -1,20 +1,22 @@
 -- TestWeb
 SET PASSWORD FOR 'wordpress'@'localhost' = PASSWORD('XaTruwAW5aVu4way');
 SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
-SET PASSWORD FOR 'trac'@'localhost' = PASSWORD('pR4JASwapudrUc39');
 SET PASSWORD FOR 'www-data'@'localhost' = PASSWORD('rutrufabRetHa9aT');
+FLUSH PRIVILEGES;
+
 
 -- TestMail
 SET PASSWORD FOR 'dbmail'@'localhost' = PASSWORD('Thu9unuwret9ad6w');
 SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
-SET PASSWORD FOR 'roundcube'@'localhost' = PASSWORD('xatajaj84ha6Ase6');
-SET PASSWORD FOR 'postfix'@'localhost' = PASSWORD('TarecRE5Ec62cuQu');
+FLUSH PRIVILEGES;
+
 
 -- 
-SET PASSWORD FOR ''@'localhost' = PASSWORD('Fra7h4WrePrE2h4d');
+SET PASSWORD FOR 'xbmc'@'localhost' = PASSWORD('Fra7h4WrePrE2h4d');
 SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
-SET PASSWORD FOR ''@'localhost' = PASSWORD('WRaxaFap6enacRAw');
-SET PASSWORD FOR ''@'localhost' = PASSWORD('tamaswE5aBuwrebU');
+FLUSH PRIVILEGES;
+
 
 -- Test
 SET PASSWORD FOR 'bacula-backup'@'localhost' = PASSWORD('');
+FLUSH PRIVILEGES;
