c4d94bd50c191c8e145e3a70eb8083a84894a2b7
Joe Nyland <joenyland@me.com>
2012-10-26 22:46:42 +0000

    Work on ticket #26.

--- Scripts/branches/mythtv-email-notify/mythtv-email-notify.py
+++ Scripts/branches/mythtv-email-notify/mythtv-email-notify.py
@@ -3,6 +3,7 @@
 import argparse
 import BeautifulSoup
 import urllib2
+from socket import getfqdn
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
@@ -10,18 +11,29 @@ parser.add_argument("chanid", type=int, help="The %%CHANID%% for the recording t
 parser.add_argument("starttime", help="The %%STARTTIMEISOUTC%% for the recording to query for")
 args = parser.parse_args()
 
-url = "http://:6544/Dvr/GetRecorded?StartTime=" + args.starttime + "&ChanId=" + str(args.chanid)
+server = getfqdn()
+url = "http://" + server + ":6544/Dvr/GetRecorded?StartTime=" + args.starttime + "&ChanId=" + str(args.chanid)
 
 response = urllib2.urlopen(url)
 html_data = response.read()
 response.close()
 
+print getfqdn()
+
 #soup = BeautifulSoup(''.join(html_data))
 
 # Verbose output:
 if args.verbose:
-	print html_data
-	print
-	print url
-	print args.chanid
-	print args.starttime
+	print """
+__________________________________________________________________________________________
+Verbose output:
+HTML: 
+%s
+URL: %s
+Channel ID: %s
+Start Time: %s""" %(html_data, url, str(args.chanid), args.starttime)
+
+"""
+TO DO:
+Handle urllib2.urlopen exceptions.
+"""
