138eaf48af7ece7a506103b3a0445ede47ed2b24
Joe Nyland <joenyland@me.com>
2012-10-29 13:11:59 +0000

    Removed mythtv-email-notify branch used for ticket #88.

--- Scripts/branches/mythtv-email-notify/Desktop Console.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 10
-gnome-terminal --working-directory=~ --window-with-profile=DesktopConsole --geometry=50x40+200+145 &
-devilspie &
--- Scripts/branches/mythtv-email-notify/Unmount Mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-gksudo "umount /media/Mac"
-exit
