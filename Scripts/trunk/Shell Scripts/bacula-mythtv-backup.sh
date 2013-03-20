6c3dc0c4bc069ac190e8b5ab917cc12a65807b6c
Joe Nyland <joenyland@me.com>
2013-03-20 12:03:48 +0000

    Updated bacula-mythtv-backup.sh script to make it compatible with permission changes on Server1.

--- Scripts/trunk/Shell Scripts/bacula-mythtv-backup.sh
+++ Scripts/trunk/Shell Scripts/bacula-mythtv-backup.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 # Script to sync MythTV recordings from RAID array on /mnt/data to USB backup HDD on /mnt/backup1.
-# Note: This script is called by Bacula
 
 # General settings
-MOUNT=/mnt/data/mythtv														# Where the MythTV recordings directory is located.
-BACKUPDRIVE=/mnt/backup1													# Where the USB backup HDD is mounted.
-
-# MythTV overrides
-OVER="--exclude='livetv/*'"													# MythTV specific overrides to Rsync (e.g. excluded directories).
+SRC=/mnt/data/mythtv                                            # The directory containing the MythTV recordings directory.
+DST=/mnt/backup1/MythTV                                         # Destination directory
 
 # Rsync settings
-STDSWITCHES="-OvruEthm --delete"											# Standard Rsync switches.
-STDEXCLUDE="--exclude='._*' --exclude='.AppleDB*' --exclude='lost+found' "	# Standard excluded files.
+SWITCHES="-OvruEthm --delete"                                   # Rsync switches.
+EXCLUDE="--exclude=._* --exclude=.AppleDB* --exclude=livetv/"   # Excluded files.
 
-SRCDIR=MythTV
-SRC=$MOUNT
-DSTDIR=$SRCDIR
-DST=$BACKUPDRIVE/$DSTDIR
-
-rsync $STDSWITCHES $SRC/ $DST/ $STDEXCLUDE $OVER
+rsync $SWITCHES $SRC/ $DST/ $EXCLUDE
