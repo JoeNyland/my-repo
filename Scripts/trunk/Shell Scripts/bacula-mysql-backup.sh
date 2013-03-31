f90d90a1e406b0f2c4086319c99b7db89c032581
Joe Nyland <joenyland@me.com>
2013-03-31 16:30:48 +0000

    Improved cleanup routine in `bacula-mysql-backup.sh` script.

--- Scripts/trunk/Shell Scripts/bacula-mysql-backup.sh
+++ Scripts/trunk/Shell Scripts/bacula-mysql-backup.sh
@@ -8,39 +8,13 @@ MYSQLCONF=/etc/mysql/my.cnf
 BINLOGDIR=/mnt/db/mysql/binlog
 BINLOGPREFIX=mysql-bin
 
+# Main script body
 SCRIPTNAME=`basename $0`
 HOST=`hostname -s`
 HOME=`grep \`whoami\` /etc/passwd | awk -F":" '{print $6}'`
-
 LEVEL=`echo $1 | awk '{print tolower($0)}'`
-
 DATE=`date +%d-%m-%Y`
 TIME=`date +%H-%M`
-
-if [[ "$2" == "cleanup" ]]; then
-	if [[ -d ${DST} ]]; then
-		echo "Cleaning up files.";
-		if rm -rf $DST; then
-			echo "Removed temporary files from ${DST}";
-		  else
-		  echo "Failed to remove temporary files from ${DST}";
-		fi
-	else
-		echo "No files to be cleaned.";
-	fi
-	exit 0;
-fi
-
-if [ -z "$LEVEL" ]; then
-	echo "[ERROR]";
-	echo "You have not provided the required information to the script.";
-	echo "Please use the following syntax: $SCRIPTNAME [Level]";
-	echo "Or:";
-	echo "$SCRIPTNAME [Level] cleanup";
-	exit 1000;
-fi
-
-mkdir -p ${DST}
 
 if [ -z "$HOST" ]; then
 	echo "Cannot determine system hostname.";
@@ -49,12 +23,14 @@ fi
 
 case $LEVEL in
 full|differential)
-	# FULL BACKUP (will also run the same backup for a differential level job.)
-	# If the supplied credentials are vaild, then perform a FULL database dump to ${DST}/${HOST}_${db}.sql.bz2
+	# FULL backup
+	# Check that we can connect to MySQL.
 	if echo 'show databases;' | mysql --defaults-extra-file=$HOME/.my.cnf -s >/dev/null
 	then
 		echo "Performing full backup of MySQL databases from $HOST... "
-				if mysqldump --defaults-extra-file=$HOME/.my.cnf --all-databases --master-data --delete-master-logs --flush-logs > ${DST}/${HOST}_${DATE}_${TIME}.sql.dmp
+			# Dump all databases.
+			mkdir -p ${DST}
+			if mysqldump --defaults-extra-file=$HOME/.my.cnf --all-databases --master-data --delete-master-logs --flush-logs > ${DST}/${HOST}_${DATE}_${TIME}.dmp.sql
 				then
 					echo "Completed full backup of MySQL databases from $HOST.";
 					exit 0
@@ -70,15 +46,17 @@ full|differential)
 		exit 1004;
 	fi;;
 incremental)
-	# INCREMENTAL BACKUP
-	# If binary transaction logging is enabled in ${MYSQLCONF}, then the script will copy the transaction logs from ${BINLOGDIR} to ${DST}, ready to be backed up.
+	# INCREMENTAL backup
 	echo "Incremental MySQL backup selected for ${HOST}";
-	if grep "log_bin" ${MYSQLCONF} | egrep -v '^(#|$)' | grep "${BINLOGDIR}/${BINLOGPREFIX}" > /dev/null # If this returns 0, then binary logging appears to be enabled for MySQL.
+	# If this returns 0, then binary logging appears to be enabled for MySQL, so incremental backup is possible.
+	if grep "log_bin" ${MYSQLCONF} | egrep -v '^(#|$)' | grep "${BINLOGDIR}/${BINLOGPREFIX}" > /dev/null
 	then
+		# Check that we can connect to MySQL.
 		if echo 'show databases;' | mysql --defaults-extra-file=$HOME/.my.cnf -s >/dev/null
 			then
+				# Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
 				echo "Flushing transaction logs for MySQL databases on ${HOST} to prepare for backup."
-				mysqladmin --defaults-extra-file=$HOME/.my.cnf flush-logs; # Flushes the current logs, so that MySQL closes it's current file cleanly, then starts writing any new transactions to a new log file.
+				mysqladmin --defaults-extra-file=$HOME/.my.cnf flush-logs;
 				echo "MySQL binary transaction logs for databases on $HOST have been flushed and are ready to be backed up.";
 				exit 0
 			else
@@ -93,15 +71,30 @@ incremental)
 		echo "You must enable binary transaction logging in MySQL for incremental backups to work.";
 		exit 1006;
 	fi;;
+cleanup)
+	# Cleanup full backup file after backup.
+	if [[ -d ${DST} ]]; then
+		echo "Cleaning up files.";
+		if rm -rf $DST; then
+			echo "Removed temporary files from ${DST}";
+			exit 0;
+		  else
+		  echo "Failed to remove temporary files from ${DST}";
+		  exit 1008;
+		fi
+	else
+		exit 0;
+	fi;;	
 *)
-	echo "[ERROR]"
-	echo "You have not specified the backup level variable in the client-run-before-job resource definition."
-	echo "Please use the following syntax: $SCRIPTNAME [Level]";
+	echo "[ERROR]";
+	echo "You have not provided the required information to the script.";
+	echo "Please use the following syntax:";
+	echo "$SCRIPTNAME [Level]";
 	echo "Or:";
-	echo "$SCRIPTNAME [Level] cleanup";
-	exit 1005;;
+	echo "$SCRIPTNAME cleanup";
+	exit 1000;;
 esac
 
 echo "[ERROR]"
-ehco "Undefined error occurred."
-exit 1008
+ehco "An undefined error occurred."
+exit 1005
