f32dbfb8a7bf20cc8ecb34d9e961e77b0fd717db
Joe Nyland <joenyland@me.com>
2013-09-29 13:26:41 +0000

    Work on ticket #234.

--- scripts/bash/transmission-email-notify.sh
+++ scripts/bash/transmission-email-notify.sh
@@ -12,7 +12,7 @@
 			padding: 0;
 			height: 100%;
 		}
-		
+
 		body {
 			font: 100% "lucida grande", Tahoma, Verdana, Arial, Helvetica, sans-serif; /* Resets 1em to 10px */
 			font-size: medium;
@@ -22,42 +22,42 @@
 			margin: 0 15px 30px;
 			overflow: hidden;
 		}
-		
+
 		a {
 			outline: 0;
 		}
-		div#banner { 
-			position: fixed; 
-			top: 0; 
-			left: 0; 
+		div#banner {
+			position: fixed;
+			top: 0;
+			left: 0;
 			background-color: #B9B9B9; /* fallback color if gradients are not supported */
-			background-image: -webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#A7A7A7)); 
-			background-image: -webkit-linear-gradient(top, #C9C9C9, #A7A7A7); 
-			background-image:    -moz-linear-gradient(top, #C9C9C9, #A7A7A7); 
-			background-image:     -ms-linear-gradient(top, #C9C9C9, #A7A7A7); 
-			background-image:      -o-linear-gradient(top, #C9C9C9, #A7A7A7); 
+			background-image: -webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#A7A7A7));
+			background-image: -webkit-linear-gradient(top, #C9C9C9, #A7A7A7);
+			background-image:    -moz-linear-gradient(top, #C9C9C9, #A7A7A7);
+			background-image:     -ms-linear-gradient(top, #C9C9C9, #A7A7A7);
+			background-image:      -o-linear-gradient(top, #C9C9C9, #A7A7A7);
 			background-image:         linear-gradient(top, #C9C9C9, #A7A7A7); /* standard, but currently unimplemented */
-			width: 100%; 
-			margin: 0 auto; 
-			padding: 15px; 
+			width: 100%;
+			margin: 0 auto;
+			padding: 15px;
      	}
-     	div#main-content { 
+     	div#main-content {
 			padding-top: 65px;
 		}
-		div#banner-bottom { 
-			position: fixed; 
-			bottom: 0; 
-			left: 0; 
+		div#banner-bottom {
+			position: fixed;
+			bottom: 0;
+			left: 0;
 			background-color: #B9B9B9; /* fallback color if gradients are not supported */
-			background-image: -webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#A7A7A7)); 
-			background-image: -webkit-linear-gradient(top, #C9C9C9, #A7A7A7); 
-			background-image:    -moz-linear-gradient(top, #C9C9C9, #A7A7A7); 
-			background-image:     -ms-linear-gradient(top, #C9C9C9, #A7A7A7); 
-			background-image:      -o-linear-gradient(top, #C9C9C9, #A7A7A7); 
+			background-image: -webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#A7A7A7));
+			background-image: -webkit-linear-gradient(top, #C9C9C9, #A7A7A7);
+			background-image:    -moz-linear-gradient(top, #C9C9C9, #A7A7A7);
+			background-image:     -ms-linear-gradient(top, #C9C9C9, #A7A7A7);
+			background-image:      -o-linear-gradient(top, #C9C9C9, #A7A7A7);
 			background-image:         linear-gradient(top, #C9C9C9, #A7A7A7); /* standard, but currently unimplemented */
-			width: 100%; 
-			margin: 0 auto; 
-			padding: 5px; 
+			width: 100%;
+			margin: 0 auto;
+			padding: 5px;
 			color: #E1E1E1;
 			font-size: x-small;
      	}
@@ -68,11 +68,11 @@
 	<div id=banner>
 		<a href="http://www.transmissionbt.com"><img src="http://www.transmissionbt.com/images/transmission.png" alt="Transmission" height="32px" width="310px"/><img src="http://www.transmissionbt.com/images/transmission-74x74.png" alt="Transmission" height="48px" width="48px"/></a>
 	</div>
-    
+
     <div id=main-content>
 		</br>
 		<h3>Transmission has completed downloading "$TR_TORRENT_NAME"</h3>
-	
+
 		<p>
 		Your download can be found on ,
 		</br>in the following location:
@@ -81,7 +81,7 @@
 		$TR_TORRENT_DIR/$TR_TORRENT_NAME
 		</p>
 	</div>
-	
+
 	<div id=banner-bottom>
         (v$TR_APP_VERSION)
     </div>
