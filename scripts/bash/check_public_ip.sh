1ae423badbd2fd54ac556d68eb64b68582a5f6dc
Joe Nyland <joenyland@me.com>
2014-02-10 23:16:47 +0000

    Created a quick public IP check script.

---
+++ scripts/bash/check_public_ip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+recipient=""
+current_ip=`curl -s icanhazip.com`
+last_ip=$current_ip
+
+clear
+while true
+do
+	current_ip=`curl -s icanhazip.com`
+	if [[ $current_ip == $last_ip ]]
+	then
+		echo -ne "`date` [INFO] Current public IP is $current_ip"\\r
+	else
+		date=`date`
+		echo 
+		echo 
+		echo "$date [WARN] Public IP address has changed! Public IP is now $current_ip"
+		echo "$date [WARN] Public IP address has changed! Public IP is now $current_ip" | mail -s "[WARN] Public IP address has changed!" $recipient
+		echo 
+	fi
+	last_ip=$current_ip
+	sleep 60
+done
