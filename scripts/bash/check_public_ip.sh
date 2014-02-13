e2036686e9b4a5cf07067f39f48dac027e634998
Joe Nyland <joenyland@me.com>
2014-02-13 21:43:41 +0000

    Improved check_public_ip.sh:

--- scripts/bash/check_public_ip.sh
+++ scripts/bash/check_public_ip.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
-recipient=""
-current_ip=`curl -s --connect-timeout 300 --max-time 300 icanhazip.com`
-last_ip=$current_ip
+# Define the recipient of email notifications from the input arg to the script:
+recipient=$1
+
+# Define the get-ip() function to obtain the current IP address:
+get-ip() {
+    current_ip=`curl -s --connect-timeout 300 --max-time 300 icanhazip.com`
+
+    if [ -n "$current_ip" ] # Check if curl has returned an IP
+    then
+        echo $current_ip    # If so, then we echo the IP out from the function
+        return
+    else
+        sleep 5             # Else, we sleep for 5s
+        get-ip              # then re-run the function to try again
+    fi
+}
+
+current_ip=`get-ip`         # Set the initial value of $current_ip
+last_ip=$current_ip         # and $last_ip to the same value
 
 clear
-while true
+while true                  # Loop until break with CTRL-C
 do
-	current_ip=`curl -s --connect-timeout 300 --max-time 300 icanhazip.com`
-	if [[ $current_ip == $last_ip ]]
+    current_ip=`get-ip`     # Get the current IP
+    if [[ $current_ip == $last_ip ]]    # If the current IP is the same as the last IP
 	then
-		echo -ne "`date` [INFO] Current public IP is $current_ip"\\r
+		echo -ne "`date` [INFO] Current public IP is $current_ip"\\r    # Update the console
 	else
-		date=`date`
-		echo 
-		echo 
+		date=`date`         # Else, warn the user on the console and via email
+		echo
+		echo
 		echo "$date [WARN] Public IP address has changed! Public IP is now $current_ip"
-		echo "$date [WARN] Public IP address has changed! Public IP is now $current_ip" | mail -s "[WARN] Public IP address has changed!" $recipient
-		echo 
+		if [ -n "$1" ]
+		    then
+                echo "$date [WARN] Public IP address has changed! Public IP is now $current_ip" | mail -s "[WARN] Public IP address has changed!" $recipient
+        fi
+		echo
 	fi
-	last_ip=$current_ip
-	sleep 60
+	last_ip=$current_ip     # Set the current IP as the last IP, ready for the next iteration
+	sleep 60                # Sleep for 60s until the next check
 done
