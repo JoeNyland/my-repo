9045f9579578396f7e687c0b53ca5633b40885a4
Joe Nyland <joenyland@me.com>
2014-06-29 11:20:58 +0100

    Fix indentation in bacula-check-client-online.sh

--- shell/bacula-check-client-online.sh
+++ shell/bacula-check-client-online.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Script to check whether or not a host is online, in order to decide whether a Bacula backup should be attempted.
-
-HOSTNAME=$1		# Specify the host to test (DNS hostname only).
-PORT=9102       # Specify the TCP port to check (Here we check port 9102, which is the default port for the Bacula File Daemon).
+HOSTNAME=$1 # Specify the host to test (DNS hostname only).
+PORT=9102   # Specify the TCP port to check (Here we check port 9102, which is the default port for the Bacula File Daemon).
 
 if nc -zv -w30 $HOSTNAME $PORT
 then
