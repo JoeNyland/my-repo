7ca418ad92fd0f35efe4bee72d862c6940b91c58
Joe Nyland <joenyland@me.com>
2014-07-05 17:36:05 +0100

    Default output to stdout and input to stdin, unless a file is specified

--- python/twitter-friends.py
+++ python/twitter-friends.py
@@ -13,11 +13,11 @@ import twitter
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Import or Export followed Twitter friends.')
 parser.add_argument('action', choices=['import', 'export'], help='Choose whether to import or export the list of friends followed')
-parser.add_argument('file', help='Input/Output file containing friends, defaults to friends.txt', default='friends.txt', nargs='?')
 parser.add_argument('--api-key', required=True, dest='api_key', help='Twitter API key')
 parser.add_argument('--api-secret', required=True, dest='api_secret', help='Twitter API secret')
 parser.add_argument('--token', required=True, help='Twitter Access token')
 parser.add_argument('--token-secret', required=True, dest='token_secret', help='Twitter Access token secret')
+parser.add_argument('--file', '-f', help='Input/Output file containing friends, defaults to friends.txt')
 parser.add_argument('--enable-notifications', dest='notifications', help='When importing, use this argument to enable notifications for the new friend(s) being created', action='store_true')
 parser.add_argument('--verbose', '-v', action='store_true')
 args = parser.parse_args()
@@ -35,25 +35,28 @@ def get_user(api):
     user = api.VerifyCredentials()
     return user
 
-def input_file():
+def out_file():
     # Check to see if the user defined an input/output file
-    try:
-        args.file
-    except NameError:
-        args.file = None
-    
-    if args.file is None:
-        # If not defined, default to twitter-friends.txt in $PWD
-        friends_file = 'twitter-friends.txt'
+    if args.file == None:
+        return sys.stdout
     else:
-        friends_file = args.file
-    
-    return friends_file
+        return open(args.file, 'w')
 
-def export_friends(api, user, friends_file):
-    # Prepare the file for writing
-    friends_file = open(friends_file, 'w')
-    
+def in_file():
+    # Check to see if the user defined an input/output file
+    if args.file == None:
+        return sys.stdin
+    else:
+        return open(args.file, 'r')
+
+def export_friends(api, user):
+    # If out_file() returns std.out, then redirect std.out to std.err
+    if out_file().name == '<stdout>':
+        output = sys.stdout         # Save stdout as output
+        sys.stdout = sys.stderr     # Now, redirect stdout to stderr (for printing messages)
+    else:
+        output = out_file()         # Not outputting to stdout, so just set output to the file
+
     # Get the list of friends that this user is following
     friends = reversed(api.GetFriends())
     
@@ -61,27 +64,22 @@ def export_friends(api, user, friends_file):
     
     # For each friend followed, write their ID to the file
     for friend in friends:
-        screen_name = friend.screen_name
-        id = str(friend.id) + '\n'
-        friends_file.write(id)
+        output.write(str(friend.id) + '\n')
         if args.verbose:
-            print 'Exported friendship with: @' + screen_name + '.'
+            print 'Exported friendship with: @' + friend.screen_name + '.'
     
     print 'Finished exporting @' + user.GetScreenName() + "'s friends."
     
-    friends_file.close()
+    output.close()
     api.ClearCredentials()
     
     return True
 
-def import_friends(api, user, friends_file, notifications=False):
-    # Prepare the file for reading
-    friends_file = open(friends_file, 'r')
-    
+def import_friends(api, user, notifications=False):
     print 'Importing @' + user.GetScreenName() + "'s friends..."
     
     # Follow each friend in file
-    for line in friends_file:
+    for line in in_file().read():
         friend_id = line.rstrip()
         friend = api.CreateFriendship(friend_id, follow=notifications)
         if args.verbose:
@@ -89,7 +87,7 @@ def import_friends(api, user, friends_file, notifications=False):
     
     print 'Finished importing @' + user.GetScreenName() + "'s friends."
     
-    friends_file.close()
+    in_file().close()
     api.ClearCredentials()
     
     return True
@@ -101,16 +99,13 @@ def main():
     # Initialise User object
     user = get_user(api)
     
-    # Set the file name
-    friends_file = input_file()
-    
     # Import or export?
     if args.action == 'export':
         # User wants to export friends
-        export_friends(api, user, friends_file)
+        export_friends(api, user)
     elif args.action == 'import':
         # User wants to import friends
-        import_friends(api, user, friends_file, bool(args.notifications))
+        import_friends(api, user, bool(args.notifications))
     else:
         # This should never happen, but...
         raise Exception("Undefined action: Should be 'import' or 'export'")
