ffd2ed7f5f717cd682145f9882b18e627e7836ea
Joe Nyland <joenyland@me.com>
2014-08-06 11:08:17 +0100

    Fix flac2mp3.sh

--- flac2mp3.sh
+++ flac2mp3.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-if [ -f *.flac ]
-then
-    for file in *.flac
-    do
-        ffmpeg -loglevel error -i "$file" -qscale:a 0 "${file[@]/%flac/mp3}"
-        echo "Converted ${file}..."
-    done
-    tput bel
-    echo "Completed converting $(ls *.flac | wc -l) FLAC file(s)."
-else
-    echo "There are no FLAC files in the current directory: $(pwd)"
-fi
+for file in *.flac
+do
+    ffmpeg -loglevel error -i "$file" -qscale:a 0 "${file[@]/%flac/mp3}"
+    echo "Converted ${file}..."
+done
+tput bel
+echo "Completed converting $(ls *.flac | wc -l) FLAC file(s)."
+
