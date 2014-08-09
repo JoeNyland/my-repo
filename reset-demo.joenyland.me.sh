f8d42774a570bab4074ead410ee693413ce3e3ea
Joe Nyland <joenyland@me.com>
2014-08-09 09:32:57 +0100

    Add demo data SQL script to reset-demo.joenyland.me.sh

--- reset-demo.joenyland.me.sh
+++ reset-demo.joenyland.me.sh
@@ -16,3 +16,4 @@ git --work-tree $SITE_ROOT --git-dir ${SITE_ROOT}/.git clean -d --force
 mysql -u${USER_NAME} -p${PASSWORD} -e 'DROP DATABASE IF EXISTS demo_alpha_crm;' && \
 mysql -u${USER_NAME} -p${PASSWORD} < ${SITE_ROOT}/app/db/create-schema.sql && \
 mysql -u${USER_NAME} -p${PASSWORD} < ${SITE_ROOT}/app/db/create-users-and-groups.sql
+mysql -u${USER_NAME} -p${PASSWORD} < ${SITE_ROOT}/app/db/create-demo-data.sql
