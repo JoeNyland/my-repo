eeac72d59128f2c96cbd672674c2426a725ba061
Joe Nyland <joenyland@me.com>
2014-09-03 18:52:11 +0100

    Update reset-demo.joenyland.me.sh for RDS

--- reset-demo.joenyland.me.sh
+++ reset-demo.joenyland.me.sh
@@ -2,7 +2,8 @@
 
 # Reset DBs for demo.joenyland.me sites
 
-# MySQL user config
+# MySQL variables
+HOST='rds1.aws.joenyland.me'
 USER_NAME='demo'
 PASSWORD='VBMiElHFoGv7aZ7vsSu8wpQcCoRntajV'
 
@@ -13,7 +14,7 @@ git --work-tree $SITE_ROOT --git-dir ${SITE_ROOT}/.git fetch && \
 git --work-tree $SITE_ROOT --git-dir ${SITE_ROOT}/.git reset --hard origin/demo && \
 git --work-tree $SITE_ROOT --git-dir ${SITE_ROOT}/.git clean -d --force
 
-mysql -u${USER_NAME} -p${PASSWORD} -e 'DROP DATABASE IF EXISTS demo_alpha_crm;' && \
-mysql -u${USER_NAME} -p${PASSWORD} < ${SITE_ROOT}/app/db/create-schema.sql && \
-mysql -u${USER_NAME} -p${PASSWORD} < ${SITE_ROOT}/app/db/create-users-and-groups.sql
-mysql -u${USER_NAME} -p${PASSWORD} < ${SITE_ROOT}/app/db/create-demo-data.sql
+mysql -h $HOST -u $USER_NAME -p${PASSWORD} -e 'DROP DATABASE IF EXISTS demo_alpha_crm;' && \
+mysql -h $HOST -u $USER_NAME -p${PASSWORD} < ${SITE_ROOT}/app/db/create-schema.sql && \
+mysql -h $HOST -u $USER_NAME -p${PASSWORD} < ${SITE_ROOT}/app/db/create-users-and-groups.sql
+mysql -h $HOST -u $USER_NAME -p${PASSWORD} < ${SITE_ROOT}/app/db/create-demo-data.sql
